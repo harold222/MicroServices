eb8ca42dbea3dd9dcb5b8728c5e13088c0a52c48
Harold Pedraza <t1harole@hotmail.com>
2022-03-15 18:54:43 -0500

    -

---
+++ blockchain/blockchain/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace blockchain
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => this.Configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                _ = app.UseDeveloperExceptionPage();
+            }
+
+            _ = app.UseSwagger();
+            _ = app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlockChain V1");
+                c.OAuthClientId("swagger_blockchain");
+                c.OAuthAppName("Swagger for BlockChain");
+                c.OAuthUsePkce();
+            });
+
+            _ = app.UseRouting();
+        }
+    }
+}
